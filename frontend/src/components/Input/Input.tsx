5321d0e67d7a851289effe2f19f3e6bd881b6205
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-01-02 18:35:01 +0700

    Button, Input, Box components

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,9 +1,18 @@
+import { InputHTMLAttributes } from 'react';
 import classes from './Input.module.scss';
 
-const Input = () => {
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string
+};
+
+
+const Input = (
+  {label, ...otherProps} : InputProps
+) => {
   return (
     <div className={classes.root}>
-      Input
+      <label>{label}</label>
+      <input {...otherProps} />
     </div>
   );
 };
--- frontend/src/features/authentication/pages/Register/Register.tsx
+++ frontend/src/features/authentication/pages/Register/Register.tsx
@@ -1,5 +1,7 @@
 
 import Box from "../../../../components/Box/Box";
+import Button from "../../../../components/Button/Button";
+import Input from "../../../../components/Input/Input";
 import Layout from "../../../../components/layout/Layout";
 import classes from "./Register.module.scss";
 
@@ -11,7 +13,9 @@ const Register = () => {
             <h1>Register</h1>
             <p>Make the most of your professional life.</p>
             <form>
-                
+                <Input type="email" id="email" label="Email" />
+                <Input type="password" id="email" label="Password" />
+                <Button style={{width: '100%'}} type="submit">Agree & Join</Button>
             </form>
         </Box>
       </div>
